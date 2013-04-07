52891b2442043643e5a5115f13e749c26a1d5f9e
Nilesh Karia <nileshkaria@gmail.com>
2013-04-07 19:06:23 -0400

    Compiled static library and test for crypto++ standard cryptographic libs.

---
+++ cryptopp562/StdAfx.h
@@ -0,0 +1,11 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+#pragma once
+
+#ifdef _DEBUG
+#  pragma comment ( lib, "cryptlibd" )
+#else
+#  pragma comment ( lib, "cryptlib" )
+#endif
---
+++ cryptopp562/SymmetricCipher.h
@@ -0,0 +1,26 @@
+#ifndef __BLOCK_CIPHERS__
+#define __BLOCK_CIPHERS__
+
+#include "aes.h"
+#include "blowfish.h"
+#include "camellia.h"
+#include "cast.h"
+#include "des.h"
+#include "gost.h"
+#include "idea.h"
+#include "mars.h"
+#include "rc2.h"
+#include "rc5.h"
+#include "rc6.h"
+#include "Rijndael.h"
+#include "safer.h"
+#include "serpent.h"
+#include "shacal2.h"
+#include "shark.h"
+#include "skipjack.h"
+#include "tea.h"
+#include "3way.h"
+#include "twofish.h"
+#include "tea.h"
+
+#endif  // __BLOCK_CIPHERS__
